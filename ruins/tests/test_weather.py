7b0be393ec4ba8130b850ea857090ce389dded3f
Mirko Mälicke <mirko@maelicke-online.de>
2022-03-03 09:19:39 +0100

    added another test

--- ruins/tests/test_weather.py
+++ ruins/tests/test_weather.py
@@ -22,3 +22,18 @@ def test_climate_indices():
 
     # run
     weather.climate_indices(w, c)
+
+
+def test_climate_indi():
+    """Test climate indi function"""
+    conf = get_test_config()
+    dm = DataManager(**conf)
+
+    weath = dm['weather'].read()
+
+    # make an arbitrary selection
+    w = weath['coast'].sel(vars='Tmin').to_dataframe().dropna()
+    w.columns = ['_', 'Tmin']
+
+    # run
+    weather.climate_indi(w, 'Ice days (Tmax < 0°C)')

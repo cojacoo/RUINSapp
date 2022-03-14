d02caff07fdb9a9bab45cd8056dbb76948002b30
Mirko Mälicke <mirko@hydrocode.de>
2022-03-14 16:55:05 +0100

    add custom memoizing

---
+++ ruins/core/cache.py
@@ -0,0 +1,32 @@
+from typing import Callable, List
+from functools import wraps
+import hashlib
+
+
+LOCAL = dict()
+
+def _hashargs(fname, argnames):
+    h = f'{fname};' + ','.join(argnames)
+    digest = hashlib.sha256(h.encode()).hexdigest()
+
+    return digest
+
+def partial_memoize(hash_names: List[str], store: str = 'local'):
+    def func_decorator(f: Callable):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            argnames = [a for a in args if a in hash_names]
+            argnames.extend([k for k in kwargs.values() if k in hash_names])
+            # get the parameter hash
+            h = _hashargs(f.__name__, argnames)
+            
+            # check if result exists
+            if h in LOCAL:
+                return LOCAL.get(h)
+            else:
+                # process
+                result = f(*args, **kwargs)
+                LOCAL[h] = result
+                return result
+        return wrapper
+    return func_decorator

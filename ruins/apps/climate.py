0e98841af87b0a2e7bb0493957edbac6fce15a3a
Mirko Mälicke <mirko@hydrocode.de>
2022-04-16 09:40:03 +0200

    plot fix in climate explorer

--- ruins/apps/climate.py
+++ ruins/apps/climate.py
@@ -37,13 +37,56 @@ def climate_explorer(dataManager: DataManager, config: Config):
     topic_expander = st.sidebar.expander('Topic selection', expanded=True)
     topic = topic_select.topic_select(config=config, topic_list=['Warming'], expander_container=topic_expander)
 
-    # select first reference
+    # ----------------------
+    # SELECT FIRST REFERENCE
     data_select.rcp_selector(dataManager, config, title='Select first reference', expander_container=option_container, elements='__all__', layout='columns', RCP_KEY='rcp_reference_1', allow_skip=False)
-    data_select.rcp_selector(dataManager, config, title='Select second reference', expander_container=option_container, elements='__all__', layout='row', RCP_KEY='rcp_reference_2', allow_skip=False)
     cref1 = config['rcp_reference_1']
+
+    if cref1=='weather':
+        data1 = dataManager['weather'].read()
+        data1 = data1.coast
+        drngx1 = (1980,2000)
+    else:
+        data1 = dataManager['cordex_coast'].read()
+        drngx1 = (2050, 2070)
+    
+    # filter for rcps
+    if cref1 == 'rcp26':
+        data1 = data1.filter_by_attrs(RCP='rcp26')
+    elif cref1 == 'rcp45':
+        data1 = data1.filter_by_attrs(RCP='rcp45')
+    elif cref1 == 'rcp85':
+        data1 = data1.filter_by_attrs(RCP='rcp85')
+
+    # create the date range silder
+    drng1 = [pd.to_datetime(data1.isel(time=0, vars=1).time.values).year, pd.to_datetime(data1.isel(time=-1, vars=1).time.values).year]
+    datarng1 = option_container.slider('Data range', drng1[0], drng1[1], drngx1, key='dr1')
+
+    # -----------------------
+    # SELECT SECOND REFERENCE
+    data_select.rcp_selector(dataManager, config, title='Select second reference', expander_container=option_container, elements='__all__', layout='row', RCP_KEY='rcp_reference_2', allow_skip=False)
     cref2 = config['rcp_reference_2']
 
-    # TODO REFACTOR FROM HERE
+    if cref2 == 'weather':
+        data2 = dataManager['weather'].read()
+        drngx2 = (1980, 2000)
+    else:
+        data2 = dataManager['cordex_coast'].read()
+        drngx2 = (2050, 2070)
+    
+    # filter for rcps
+    if cref2 == 'rcp26':
+        data2 = data2.filter_by_attrs(RCP='rcp26')
+    elif cref2 == 'rcp45':
+        data2 = data2.filter_by_attrs(RCP='rcp45')
+    elif cref2 == 'rcp85':
+        data2 = data2.filter_by_attrs(RCP='rcp85')
+
+    # create the date range silder
+    drng2 = [pd.to_datetime(data2.isel(time=0, vars=1).time.values).year, pd.to_datetime(data2.isel(time=-1, vars=1).time.values).year]
+    datarng2 = option_container.slider('Data range', drng2[0], drng2[1], drngx2, key='dr2')
+
+    # TODO: build this into a Component
     if cliproj=='Regional':
         regaggs = ['North Sea Coast', 'Krummhörn',  'Niedersachsen', 'Inland']
         regagg = st.sidebar.selectbox('Spatial aggregation:', regaggs)
@@ -54,53 +97,14 @@ def climate_explorer(dataManager: DataManager, config: Config):
         elif regagg=='Krummhörn':
             climate = xr.load_dataset('data/cordex_krummh.nc')
 
-#    st.subheader('Climate Model Comparison')
-#    crefs = ['Weather Data', 'all RCPs', 'RCP2.6', 'RCP4.5','RCP8.5']
-#    cref1 = st.sidebar.selectbox('Select first reference:', options=crefs)
-
-    if cref1=='weather':
-        data1 = load_data('Weather')
-        data1 = data1.coast
-        drngx1 = (1980,2000)
-    elif cref1 == 'allRCP':
-        data1 = load_data('CORDEX')
-        drngx1 = (2050, 2070)
-    elif cref1 == 'rcp26':
-        data1 = load_data('rcp26','North Sea Coast')
-        drngx1 = (2050, 2070)
-    elif cref1 == 'rcp45':
-        data1 = load_data('rcp45', 'North Sea Coast')
-        drngx1 = (2050, 2070)
-    elif cref1 == 'rcp85':
-        data1 = load_data('rcp85','North Sea Coast')
-        drngx1 = (2050, 2070)
-
-    drng1 = [pd.to_datetime(data1.isel(time=0, vars=1).time.values).year, pd.to_datetime(data1.isel(time=-1, vars=1).time.values).year]
-    datarng1 = st.sidebar.slider('Data range', drng1[0], drng1[1], drngx1, key='dr1')
-
-#    cref2 = st.sidebar.selectbox('Select second reference:', options=crefs)
-    if cref2 == 'weather':
-        data2 = load_data('Weather')
-        drngx2 = (1980, 2000)
-    elif cref2 == 'allRCP':
-        data2 = load_data('CORDEX')
-        drngx2 = (2050, 2070)
-    elif cref2 == 'rcp26':
-        data2 = load_data('rcp26','North Sea Coast')
-        drngx2 = (2050, 2070)
-    elif cref2 == 'rcp45':
-        data2 = load_data('rcp45', 'North Sea Coast')
-        drngx2 = (2050, 2070)
-    elif cref2 == 'rcp85':
-        data2 = load_data('rcp85','North Sea Coast')
-        drngx2 = (2050, 2070)
-
-    drng2 = [pd.to_datetime(data2.isel(time=0, vars=1).time.values).year, pd.to_datetime(data2.isel(time=-1, vars=1).time.values).year]
-    datarng2 = st.sidebar.slider('Data range', drng2[0], drng2[1], drngx2, key='dr2')
-
-    if w_topic == 'Warming':
+    # ------------------
+    # Main Area
+    plot_opts, plot_area = st.columns((2, 8))
+    
+    # TODO: Use the actual selected topic here
+    if config['current_topic'] == 'Warming':
         navi_vars = ['Maximum Air Temperature', 'Mean Air Temperature', 'Minimum Air Temperature']
-        navi_var = st.sidebar.radio("Select variable:", options=navi_vars)
+        navi_var = plot_opts.radio("Select variable:", options=navi_vars)
         if navi_var[:4] == 'Mini':
             vari = 'Tmin'
             afu = np.min
@@ -114,13 +118,15 @@ def climate_explorer(dataManager: DataManager, config: Config):
             afu = np.mean
             ag = 'mean'
 
+        # apply aggreagtion
         dyp = data1.sel(vars=vari).to_dataframe().resample('1M').apply(afu)
         dyp = dyp.loc[(dyp.index.year>=datarng1[0]) & (dyp.index.year<datarng1[1]),dyp.columns[dyp.columns!='vars']]
         dyp2 = data2.sel(vars=vari).to_dataframe().resample('1M').apply(afu)
         dyp2 = dyp2.loc[(dyp2.index.year >= datarng2[0]) & (dyp2.index.year < datarng2[1]),dyp2.columns[dyp2.columns!='vars']]
 
-        monthlyx(dyp, dyp2, 'Temperature (°C)', 'Monthly '+ag+' in Year ('+cref1+')', 'Monthly '+ag+' in Year ('+cref2+')')
-        st.pyplot()
+        # create the plot
+        fig = monthlyx(dyp, dyp2, 'Temperature (°C)', 'Monthly '+ag+' in Year ('+cref1+')', 'Monthly '+ag+' in Year ('+cref2+')')
+        st.pyplot(fig=fig)
 
 
 def main_app(**kwargs):
